f3049bb3bd3922f9880a53e9fd435f343a602592
Youngjin Mo <dev.youngjinmo@gmail.com>
2020-11-30 13:58:01 +0900

    인자받는 컨트롤러(@RequestParam) 추가

--- src/main/java/com/inflearn/hellospringboot/controller/HelloController.java
+++ src/main/java/com/inflearn/hellospringboot/controller/HelloController.java
@@ -4,12 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
+
     @GetMapping("hello/{guest}")
     public String hello(@PathVariable String guest, Model model){
         model.addAttribute("guest", guest);
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-mvc";
     }
 }
