4aa921ee366f49b626fdab6fe06512ab3d616fde
Youngjin Mo <dev.youngjinmo@gmail.com>
2020-12-01 12:23:55 +0900

    Rename boot controller

--- src/main/java/com/inflearn/hellospringboot/HelloSpringbootApplication.java
+++ src/main/java/com/inflearn/hellospringboot/HelloSpringbootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloSpringBootApplication {
+public class HelloSpringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringBootApplication.class, args);
+		SpringApplication.run(HelloSpringbootApplication.class, args);
 	}
 
 }
