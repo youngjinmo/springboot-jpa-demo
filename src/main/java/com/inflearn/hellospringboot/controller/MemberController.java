465cd819ab2933cd7bbf277030cd284f606fe977
Youngjin Mo <dev.youngjinmo@gmail.com>
2020-12-01 19:00:31 +0900

    create mvc page

--- src/main/java/com/inflearn/hellospringboot/controller/MemberController.java
+++ src/main/java/com/inflearn/hellospringboot/controller/MemberController.java
@@ -1,8 +1,12 @@
 package com.inflearn.hellospringboot.controller;
 
+import com.inflearn.hellospringboot.domain.Member;
+import com.inflearn.hellospringboot.domain.MemberForm;
 import com.inflearn.hellospringboot.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +16,20 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/inflearn/hellospringboot/domain/MemberForm.java
@@ -0,0 +1,9 @@
+package com.inflearn.hellospringboot.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MemberForm {
+    private String name;
+}
